399bf83aefb6d30368e972fa27f923755135166a
Yang Keao <keao.yang@yahoo.com>
2019-03-29 18:57:40 +0800

    Write the basic WG algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate crossbeam_channel;
+extern crate im;
 
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::marker::PhantomData;
@@ -6,16 +7,20 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, RwLock};
 use std::thread;
 use std::thread::JoinHandle;
+use im::vector::Vector;
+use std::cmp::PartialEq;
 
 pub trait Action: Copy + Clone + 'static {
     type Request: Copy + Clone + Send + Sync;
-    type Response: Copy + Clone + Send + Sync;
+    type Response: Copy + Clone + Send + Sync + PartialEq + Default;
 }
 
 pub trait Model<A: Action, S>: Send + Sync + 'static {
     fn init() -> Self;
 
-    fn get_state() -> S;
+    fn get_state(&self) -> S;
+
+    fn from_state(state: &S) -> Self;
 
     fn invoke(&self, action: A::Request) -> A::Response;
 }
@@ -33,7 +38,6 @@ pub struct Entry<A: Action> {
 }
 
 pub struct Checker<A: Action, S, M: Model<A, S>, R: Model<A, S>> {
-    model: M,
     runner: Arc<R>,
     threads: Vec<JoinHandle<()>>,
     history: Arc<RwLock<Vec<Entry<A>>>>,
@@ -41,6 +45,66 @@ pub struct Checker<A: Action, S, M: Model<A, S>, R: Model<A, S>> {
     counter: Arc<AtomicUsize>,
     _p1: PhantomData<A>,
     _p2: PhantomData<S>,
+    _p3: PhantomData<M>,
+}
+
+pub struct ImmutableChecker<A: Action, S, M: Model<A, S>> {
+    history: Vector<Entry<A>>,
+    state: S,
+    _p1: PhantomData<M>,
+}
+
+impl<A: Action, S, M: Model<A, S>> ImmutableChecker<A, S, M> {
+    pub fn new(history: Vector<Entry<A>>, state: S) -> Self {
+        Self {
+            history,
+            state,
+            _p1: PhantomData
+        }
+    }
+    pub fn check(&self) -> bool {
+        let mut ret = false;
+        for (index, entry) in self.history.iter().enumerate() {
+            match entry.data {
+                EntryData::Invoke(invoke) => {
+                    let model = M::from_state(&self.state);
+
+                    let mut history = self.history.clone();
+                    let mut return_index = index;
+                    let mut expect_response= A::Response::default();
+                    for i in index+1..self.history.len() {
+                        if self.history[i].id == entry.id {
+                            return_index = i;
+                            match self.history[i].data {
+                                EntryData::Invoke(_) => unreachable!(),
+                                EntryData::Response(res) => expect_response = res,
+                            }
+                            break;
+                        }
+                    }
+                    history.remove(index);
+                    history.remove(return_index - 1);
+
+                    let response = model.invoke(invoke);
+                    if response == expect_response {
+                        let sub_checker: ImmutableChecker<A, S, M> = ImmutableChecker::new(history, model.get_state());
+                        let res = sub_checker.check();
+
+                        if res {
+                            ret = true;
+                            break;
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
 }
 
 impl<A: Action, S, M: Model<A, S>, R: Model<A, S>> Checker<A, S, M, R> {
@@ -54,7 +118,6 @@ impl<A: Action, S, M: Model<A, S>, R: Model<A, S>> Checker<A, S, M, R> {
             receive_history.write().unwrap().push(res);
         });
         return Self {
-            model: M::init(),
             runner: Arc::new(R::init()),
             threads: Vec::new(),
             history,
@@ -62,6 +125,7 @@ impl<A: Action, S, M: Model<A, S>, R: Model<A, S>> Checker<A, S, M, R> {
             counter: Arc::new(AtomicUsize::new(0)),
             _p1: PhantomData,
             _p2: PhantomData,
+            _p3: PhantomData,
         };
     }
     pub fn add_thread(&mut self, history: Vec<A::Request>) {
@@ -88,4 +152,11 @@ impl<A: Action, S, M: Model<A, S>, R: Model<A, S>> Checker<A, S, M, R> {
             }
         }));
     }
+    pub fn check(&self, init_state: S) -> bool {
+        let history = Vector::from(self.history.read().unwrap().clone());
+        let im_checker: ImmutableChecker<A, S, M> = ImmutableChecker::new(history, init_state);
+
+        im_checker.check()
+    }
 }
+
